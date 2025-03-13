41fdf11d6c09d8e842820b9ed113eba5b95604c8
NbglRoot <naimbgdl2003@gmail.com>
2025-03-13 21:20:48 +0100

    Update phpmailer.php
    change for protection

--- src/php/phpmailer.php
+++ src/php/phpmailer.php
@@ -26,7 +26,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'wikirootweb@gmail.com';                     //SMTP username
-    $mail->Password   = 'aturivatyttykkbi';                               //SMTP password
+    $mail->Password   = '';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption
     $mail->Port       = 587;                                    //TCP port to connect to
 
